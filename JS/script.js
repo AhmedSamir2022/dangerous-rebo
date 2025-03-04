034e7574ff5ec28bdf8cc23e8449396239ae1e3d
ollaanoor <ollaanoor65@gmail.com>
2025-03-04 12:27:49 +0200

    update name

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@ let startBtn = document.getElementById('startBtn')
 
 startBtn.addEventListener('click',()=>{
     document.getElementById('h').innerHTML = "hisham"
-    document.getElementById('o').innerHTML = "Olla AbdulNasser Ali"
+    document.getElementById('o').innerHTML = "Olla AbdulNasser"
     document.getElementById('a').innerHTML = "amr"
     document.getElementById('s').innerHTML = "samir"
 })
