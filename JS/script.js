e4e32cb296dc38145d3c9f77af8744f9fe0c0caf
Amr Sam <amr1997sam@gmail.com>
2025-03-04 12:01:31 +0200

    amr branch

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,6 @@ let startBtn = document.getElementById('startBtn')
 startBtn.addEventListener('click',()=>{
     document.getElementById('h').innerHTML = "hisham"
     document.getElementById('o').innerHTML = "olla"
-    document.getElementById('a').innerHTML = "amr"
+    document.getElementById('a').innerHTML = "amr Elsayed Ahmed"
     document.getElementById('s').innerHTML = "samir"
 })
