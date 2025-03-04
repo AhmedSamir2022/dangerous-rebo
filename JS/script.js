40300f86e23272b6d7436aecce6e6a9fe4372fab
AhmedSamir2022 <https://github.com/AhmedSamir2022>
2025-03-04 11:43:25 +0200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+let startBtn = document.getElementById('startBtn')
+
+startBtn.addEventListener('click',()=>{
+    document.getElementById('h').innerHTML = "hisham"
+    document.getElementById('o').innerHTML = "olla"
+    document.getElementById('a').innerHTML = "amr"
+    document.getElementById('s').innerHTML = "samir"
+})
