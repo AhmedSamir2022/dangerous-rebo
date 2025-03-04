a18c1300e25b045ca3c32a0f63b71d8fab0aa41c
Amr Sam <amr1997sam@gmail.com>
2025-03-04 13:05:21 +0200

    done

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,9 @@
 let startBtn = document.getElementById("startBtn");
 
-<<<<<<< HEAD
 startBtn.addEventListener("click", () => {
   document.body.classList.add("started");
   document.getElementById("h").innerHTML = "Mohamed Hesham";
   document.getElementById("o").innerHTML = "Olla AbdulNasser";
-  document.getElementById("a").innerHTML = "amr";
+  document.getElementById("a").innerHTML = "amr Elsayed Ahmed";
   document.getElementById("s").innerHTML = "Ahmed Samir";
 });
-=======
-startBtn.addEventListener('click',()=>{
-    document.getElementById('h').innerHTML = "hisham"
-    document.getElementById('o').innerHTML = "olla"
-    document.getElementById('a').innerHTML = "amr Elsayed Ahmed"
-    document.getElementById('s').innerHTML = "samir"
-})
->>>>>>> amr-branch
